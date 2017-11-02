21673b76805e127380dffec2b5e2c6753b53431a
Mikew24 <mwaldron@rogers.com>
2017-11-02 12:22:03 -0400

    part 2 finished

--- Part2.py
+++ Part2.py
@@ -1,11 +1,34 @@
 """ palindrome checker """
+class Stack:
+    def __init__(self):
+        self.stacks = []
+
+    def top(self):
+        return self.stacks[len(self.stacks)]
+
+    def pop(self):
+        return self.stacks.pop()
+
+    def push(self, num):
+        self.stacks.append(num)
+
+    def isEmpty(self):
+        if (self.stacks == []):
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return str(self.stacks)
+
+
 def palichecker(word):
     p=''
-    d=[]
+    d=Stack()
     for char in range(len(word)):
         d.push(word[char])
     for x in range(len(word)):
-      p=p+d.pop
+      p = p+d.pop()
     if(p==word): return True
     return False
-print (palichecker('tacocat'))
+print (palichecker('tacobat'))
